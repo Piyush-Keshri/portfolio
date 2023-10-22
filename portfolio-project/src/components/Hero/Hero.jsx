3dd6b242a661ba5033ffec38a7b77ff8abc03b4b
Piyush Keshri <piyushkeshri383@gmail.com>
2023-10-22 16:41:58 +0530

    Added Hero Section

---
+++ portfolio-project/src/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './Hero.module.css'
+import { getImageUrl } from '../../utils'
+const Hero = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Hi, I am Piyush</h1>
+                <p className={styles.description}> I am a Fresher Fullstack Developer with hands on Experience in NodeJs and React.
+                    Reach out to me to learn more.
+                </p>
+                <a href='mailto:piyushkeshri383@gmail.com' className={styles.contactBtn}>Contact Me!</a>
+            </div>
+            <img src={getImageUrl("hero/heroImage.png")} alt='Hero Image' className={styles.heroImg} />
+            <div className={styles.topBlur} />
+            <div className={styles.bottomBlur} />
+        </section>
+    )
+}
+
+export default Hero
--- portfolio-project/src/components/Navbar/Navbar.jsx
+++ portfolio-project/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import styles from './Navbar.module.css'
+import { useState } from 'react';
+import { getImageUrl } from '../../utils';
+
 
 const Navbar = () => {
+    const [menuOpen, setmenuOpen] = useState(false);
     return (
         <nav className={styles.navbar}>
             <a className={styles.title} href='/'>Portfolio</a>
 
             <div className={styles.menu}>
-                <ul className={styles.menuItem}>
+
+                <img className='menuBtn' src={menuOpen ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")} alt='menu-btn' onClick={() => { setmenuOpen(!menuOpen) }} />
+
+                <ul className={`${styles.menuItem} ${menuOpen && styles.menuOpen}`} onClick={() => { setmenuOpen(false) }}>
                     <li>
                         <a href='#about'>About</a>
                     </li>
                     <li>
-                        <a href='#expirence'>Expirence</a>
+                        <a href='#expirence'>Experience</a>
                     </li>
                     <li>
                         <a href='#projects'>Projects</a>
